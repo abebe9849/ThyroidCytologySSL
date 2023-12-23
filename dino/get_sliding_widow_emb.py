f31449b8d047de04c22f4caea62230b2d5895cab
Masatoshi Abe <55616956+abebe9849@users.noreply.github.com>
2023-12-23 17:11:25 +0900

    Update and rename eval_line_all_8cls__vs_human.py to get_sliding_widow_emb.py

--- dino/get_sliding_widow_emb.py
+++ dino/get_sliding_widow_emb.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 import argparse
 import json
 from pathlib import Path
